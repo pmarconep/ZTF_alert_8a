d506d167b313b48bce058451db9fd91ce53892f5
Pascual Marcone <pmarconep@gmail.com>
2024-09-26 22:56:35 -0300

    added other plot func

--- src/plots.py
+++ src/plots.py
@@ -7,7 +7,7 @@ def plot_example(dataset, sample='random', sample2='random', n=1):
         sample2 = np.random.choice(5, n)
     else:
         n = len(sample)
-         
+
     fig, ax = plt.subplots(n, 3, figsize=(5, 1.8 * n))
     
     for i in range(n):
@@ -23,6 +23,32 @@ def plot_example(dataset, sample='random', sample2='random', n=1):
         
         ax[i, 2].imshow(dataset['Train']['difference'][sample[i]][sample2[i]])
         ax[i, 2].set_title(f'Difference {i + 1}')
+        ax[i, 2].set_xticks([])
+        ax[i, 2].set_yticks([])
+    
+    plt.show()
+
+def plot_example_dataset(dataset1, dataset2, dataset3, sample='random', n=1):
+    if sample == 'random':
+        sample = np.random.choice(dataset1['Train']['class'].size, n)
+    else:
+        n = len(sample)
+         
+    fig, ax = plt.subplots(n, 3, figsize=(5, 1.8 * n))
+    
+    for i in range(n):
+        ax[i, 0].imshow(dataset1['Train']['images'][sample[i]])
+        ax[i, 0].set_title(f'Low Res {i + 1}')
+        ax[i, 0].set_xticks([])
+        ax[i, 0].set_yticks([])
+        
+        ax[i, 1].imshow(dataset2['Train']['images'][sample[i]])
+        ax[i, 1].set_title(f'Mid Res {i + 1}')
+        ax[i, 1].set_xticks([])
+        ax[i, 1].set_yticks([])
+        
+        ax[i, 2].imshow(dataset3['Train']['images'][sample[i]])
+        ax[i, 2].set_title(f'High Res {i + 1}')
         ax[i, 2].set_xticks([])
         ax[i, 2].set_yticks([])
     
