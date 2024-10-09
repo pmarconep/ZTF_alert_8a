766bd554c8b571175e78a7706f18c1f0cb6470e0
jpcv27 <juan.contreras.v@ug.uchile.cl>
2024-10-09 17:09:13 -0300

    Decoder model alpha version

--- src/model/test/vae.py
+++ src/model/test/vae.py
@@ -31,3 +31,59 @@ class Encoder(nn.Module):
         mu = self.fc_mu(h)
         logvar = self.fc_logvar(h)
         return mu, logvar
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim):
+        super(Decoder, self).__init__()
+        
+        self.layers = nn.Sequential(
+            
+            # FCL
+            nn.Linear(latent_dim, 64*6*6),
+            nn.ReLU(),
+            
+            # Reshape to (64, 6, 6)
+            nn.Unflatten(1, (64, 6, 6)),
+            
+            #1st Convolutional Layer
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            
+            #2nd Convolutional Layer
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            
+            # Nearest Neighbour interpolation
+            nn.Upsample(size=(11,11), mode='nearest'),
+            
+            #3rd Convolutional Layer
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            
+            #4th Convolutional Layer
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            
+            # Nearest Neighbour interpolation
+            nn.Upsample(size=(21,21), mode='nearest'),
+            
+            #5th Convolutional Layer
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),  
+            
+            #6th Convolutional Layer
+            nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1), # Tengo dudas con esta última capa
+            nn.ReLU()            
+        )
+    def forward(self, x):
+        return self.layers(x)   
+        
+def reparametrize(mu, logvar):
+    std = torch.exp(0.5 * logvar)
+    eps = torch.randn_like(std)
+    return mu + eps * std
