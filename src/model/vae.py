af9d00b8a1b0ae35f9ac8d47a02e745c17df5bc9
Pascual Marcone <pmarconep@gmail.com>
2024-10-15 11:13:11 -0300

    added variable for n_channels

--- src/model/vae.py
+++ src/model/vae.py
@@ -4,11 +4,11 @@ import torch.optim as optim
 from torch.nn import functional as F
     
 class VAE(nn.Module):
-    def __init__(self, latent_dim):
+    def __init__(self, latent_dim, n_channels):
         super(VAE, self).__init__()
         
         self.encoder = nn.Sequential(
-            nn.Conv2d(2, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(n_channels, 64, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
@@ -67,8 +67,8 @@ class VAE(nn.Module):
             nn.ReLU(),  
             
             #6th Convolutional Layer
-            nn.Conv2d(64, 2, kernel_size=3, stride=1, padding=1), # Tengo dudas con esta última capa
-            nn.ReLU()            
+            nn.Conv2d(64, n_channels, kernel_size=3, stride=1, padding=1), # Tengo dudas con esta última capa
+            nn.ReLU()
         )
         
         self.fc_sigma = nn.Sequential(
