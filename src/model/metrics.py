f52aef5e7ec2704d1d42e833796b9ce8ad6707a7
Pascual Marcone <pmarconep@gmail.com>
2024-10-16 17:49:21 -0300

    metrics fixes

--- src/model/metrics.py
+++ src/model/metrics.py
@@ -52,43 +52,18 @@ def conf_matrix_given_threshold(true_labels, prediction, threshold):
     TN, FP, FN, TP = confusion_matrix(true_labels, probabilities_with_threshold).ravel()
     return TN, FP, FN, TP
 
-def show_curves(curves):
-    fig, ax = plt.subplots(1, 2, figsize=(13, 5))
+def show_curves(curves, models):
+    fig, ax = plt.subplots(1, len(curves), figsize=((13/2)*len(curves), 5))
     fig.set_facecolor('white')
 
     epochs = np.arange(len(curves["val_loss"])) + 1
 
-    ax[0].plot(epochs, curves['val_loss'], label='validation')
-    ax[0].plot(epochs, curves['train_loss'], label='training')
-    ax[0].set_xlabel('Epoch')
-    ax[0].set_ylabel('Loss')
-    ax[0].set_title('Loss evolution during training')
-    ax[0].legend()
-
-    # ax[1].plot(epochs, curves['val_acc'], label='validation')
-    # ax[1].plot(epochs, curves['train_acc'], label='training')
-    # ax[1].set_xlabel('Epoch')
-    # ax[1].set_ylabel('Accuracy')
-    # ax[1].set_title('Accuracy evolution during training')
-    # ax[1].legend()
-
-    plt.show()
-    return fig
-
-def show_loss(curves1, curves2, curves3, curves4, curves5, curvesGauss):
-    fig, ax = plt.subplots(1, 6, figsize=(32.5, 5))
-    fig.set_facecolor('white')
-
-    epochs = np.arange(len(curves1["val_loss"])) + 1
-
-    curves = [curves1, curves2, curves3, curves4, curves5, curvesGauss]
-    
-    for i in range(6):
-        ax[i].plot(epochs, curves[i]['val_loss'], label='validation')
-        ax[i].plot(epochs, curves[i]['train_loss'], label='training')
+    for i, curve in enumerate(curves):
+        ax[i].plot(epochs, curve['val_loss'], label='validation')
+        ax[i].plot(epochs, curve['train_loss'], label='training')
         ax[i].set_xlabel('Epoch')
         ax[i].set_ylabel('Loss')
-        ax[i].set_title(f'Loss evolution during training (Model {i})')
+        ax[i].set_title(f'Loss evolution during training model {models[i].name}')
         ax[i].legend()
 
     plt.show()
@@ -96,24 +71,30 @@ def show_loss(curves1, curves2, curves3, curves4, curves5, curvesGauss):
 
 import umap
 
-def plot_umap(model, data, labels, n_neighbors, min_dist, metric, norm = True):
-    model.eval()
+def plot_umap(models, data, labels, n_neighbors, min_dist, metric, norm = True):
 
-    z = model.only_encoder(data).detach().numpy()
-    z_label = labels.detach().numpy()
+    fig, ax = plt.subplots(1, len(models), figsize=((13/2)*len(models), 5))
+    fig.set_facecolor('white')
 
-    if norm:
-        z = (z - z.mean(axis=0)) / z.std(axis=0)
+    for i, model in enumerate(models):
 
-    reducer = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, metric=metric)
-    embedding = reducer.fit_transform(z)
+        model.eval()
 
-    colors = ['red', 'blue', 'green', 'purple', 'orange']
+        z = model.only_encoder(data).detach().numpy()
+        z_label = labels.detach().numpy()
 
-    fig = plt.figure(figsize=(10, 8))
-    for i, color in enumerate(colors):
-        plt.scatter(embedding[z_label == i, 0], embedding[z_label == i, 1], c=color, label=f'Class {i}', s=5)
-    # plt.title(f'UMAP projection of the latent space of model {model.name}')
-    plt.legend()
+        if norm:
+            z = (z - z.mean(axis=0)) / z.std(axis=0)
+
+        reducer = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, metric=metric)
+        embedding = reducer.fit_transform(z)
+
+        colors = ['red', 'blue', 'green', 'purple', 'orange']
+
+        for i, color in enumerate(colors):
+            ax[i].scatter(embedding[z_label == i, 0], embedding[z_label == i, 1], c=color, label=f'Class {i}', s=5)
+        ax[i].set_title(f'UMAP projection of the latent space of model {model.name}')
+        plt.legend()
+    
     plt.show()
     return fig
