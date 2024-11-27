025ea897df6c13faae8a3a5a4402fc1728e11639
Pascual Marcone <pmarconep@gmail.com>
2024-11-27 15:09:58 -0300

    no entiendooooo

--- src/final_model.py
+++ src/final_model.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 class FinalModel(nn.Module):
-    def __init__(self, latent_dim, n_channels, rnn_type, hidden_dim, num_layers, n_classes, name = 'None', description = 'None'):
+    def __init__(self, latent_dim, n_channels, rnn_type, hidden_dim, num_layers, dropout_prob, n_classes, name = 'None', description = 'None'):
         super(FinalModel, self).__init__()
 
         self.name = name
@@ -19,47 +19,59 @@ class FinalModel(nn.Module):
         #encoder
         self.encoder = nn.Sequential(
             nn.Conv2d(n_channels, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
+            nn.LayerNorm([64, 21, 21]),
             nn.ReLU(),
+            nn.Dropout(dropout_prob),
             nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(64),
+            nn.LayerNorm([64, 11, 11]),
             nn.ReLU(),
+            nn.Dropout(dropout_prob),
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
+            nn.LayerNorm([64, 11, 11]),
             nn.ReLU(),
+            nn.Dropout(dropout_prob),
             nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(64),
+            nn.LayerNorm([64, 6, 6]),
             nn.ReLU(),
+            nn.Dropout(dropout_prob),
             nn.Flatten(),
-            nn.Linear(64*6*6, latent_dim)
+            nn.Linear(64*6*6, latent_dim),
+            nn.Dropout(dropout_prob)
         )
 
         #decoder
         self.decoder = nn.Sequential(
             nn.Linear(latent_dim, 64*6*6),
             nn.ReLU(),
+            nn.Dropout(dropout_prob),
             nn.Unflatten(1, (64, 6, 6)),
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
+            nn.LayerNorm([64, 6, 6]),
             nn.ReLU(),
+            nn.Dropout(dropout_prob),
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
+            nn.LayerNorm([64, 6, 6]),
             nn.ReLU(),
+            nn.Dropout(dropout_prob),
             nn.Upsample(size=(11,11), mode='nearest'),
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
+            nn.LayerNorm([64, 11, 11]),
             nn.ReLU(),
+            nn.Dropout(dropout_prob),
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
+            nn.LayerNorm([64, 11, 11]),
             nn.ReLU(),
+            nn.Dropout(dropout_prob),
             nn.Upsample(size=(21,21), mode='nearest'),
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),  
+            nn.LayerNorm([64, 21, 21]),
+            nn.ReLU(),
+            nn.Dropout(dropout_prob),
             nn.Conv2d(64, n_channels, kernel_size=3, stride=1, padding=1),
             nn.ReLU()
         )
 
+        # RNN
         if rnn_type == 'LSTM':
             self.rnn = nn.LSTM(input_size=latent_dim, hidden_size=hidden_dim, num_layers=num_layers, batch_first=True)
         elif rnn_type == 'GRU':
@@ -68,11 +80,8 @@ class FinalModel(nn.Module):
             self.rnn = nn.RNN(input_size=latent_dim, hidden_size=hidden_dim, num_layers=num_layers, batch_first=True)
         else:
             raise ValueError("Invalid RNN type.")
-            
-        self.fc1 = nn.Linear(hidden_dim, latent_dim)
-        self.dropout = nn.Dropout(0.5) # Valor "arbitrario"
+        self.dropout_rnn = nn.Dropout(dropout_prob)
         self.fc3 = nn.Linear(latent_dim, n_classes)
-
 
     #autoencoder
     def only_encoder(self, x):
@@ -90,35 +99,34 @@ class FinalModel(nn.Module):
     #rnn
     def rnn_encode(self, x):
         size = x.shape[0]
-        x = self.encoder(x.view(-1, self.n_channels, 21, 21))
+        x = self.only_encoder(x.view(-1, self.n_channels, 21, 21))
         x, _ = self.rnn(x.view(size, 5, self.latent_dim))
-        # x = x.view(5*size, self.latent_dim)
-        lat = nn.functional.relu(self.fc1(x))
-        mid_lat = self.dropout(lat)
-        reconstruction = self.only_decoder(mid_lat.view(-1, self.latent_dim))
+        x = self.dropout_rnn(x)
+        reconstruction = self.only_decoder(x.reshape(-1, self.latent_dim))
         return reconstruction
+
+    def rnn_latent(self, x):
+        size = x.shape[0]
+        x = self.only_encoder(x.view(-1, self.n_channels, 21, 21))
+        x, _ = self.rnn(x.view(size, 5, self.latent_dim))
+        x = self.dropout_rnn(x[:, -1, :])
+        return x
 
     def rnn_classifier(self, x):
         size = x.shape[0]
-        x = self.encoder(x.view(-1, self.n_channels, 21, 21))
+        x = self.only_encoder(x.view(-1, self.n_channels, 21, 21))
         x, _ = self.rnn(x.view(size, 5, self.latent_dim))
-        x = x[:,-1,:]
-        x = nn.functional.relu(self.fc1(x))
-        x = self.dropout(x)
+        x = self.dropout_rnn(x[:, -1, :])
         x = self.fc3(x)
         return x
     
-
-    #completo
     def forward(self, x):
-        batch_length = x.shape[0]
-        z = self.only_encoder(x.view(-1, 3, 21, 21))
-        z, _ = self.rnn(z.view(batch_length, 5, 50))
-        z = z[:,-1,:]
-        z = nn.functional.relu(self.fc1(z))
-        z = self.dropout(z)
-        z = self.fc3(z)
-        return z
+        size = x.shape[0]
+        x = self.only_encoder(x.view(-1, self.n_channels, 21, 21))
+        x, _ = self.rnn(x.view(size, 5, self.latent_dim))
+        x = self.dropout_rnn(x[:, -1, :])
+        x = self.fc3(x)
+        return x
     
 def ae_loss_function(recon_x, x):
     recon_loss = nn.functional.mse_loss(recon_x, x, reduction='mean')
--- src/final_training.py
+++ src/final_training.py
@@ -26,14 +26,13 @@ class EarlyStopping:
 def augment_data(image, y_batch):
 
     rotated_90 = torch.stack([F.rotate(img, 90) for img in image])
-    rotated_180 = torch.stack([F.rotate(img, 180) for img in image])
     rotated_270 = torch.stack([F.rotate(img, 270) for img in image])
 
     h_flip = torch.stack([F.hflip(img) for img in image])
     v_flip = torch.stack([F.vflip(img) for img in image])
 
-    imgs = torch.cat((image, rotated_90, rotated_180, rotated_270, h_flip, v_flip), dim=0)
-    labels = torch.cat((y_batch, y_batch, y_batch, y_batch, y_batch, y_batch), dim=0)
+    imgs = torch.cat((image, rotated_90, rotated_270, h_flip, v_flip), dim=0)
+    labels = torch.cat((y_batch, y_batch, y_batch, y_batch, y_batch), dim=0)
     
     # Randomize both tensors in the same way so the labels correspond to the images
     indices = torch.randperm(imgs.size(0))
@@ -78,15 +77,19 @@ def train_final_model(model,
                     param.requires_grad = False
                 else:
                     param.requires_grad = True
-
-    for name, param in model.named_parameters():
-        print(f'{name}: {param.requires_grad}')
+        else:
+            for name, param in model.named_parameters():
+                param.requires_grad = True
 
     if use_gpu:
         model = model.cuda()
 
     if early_stop != False:
         early_stopping = EarlyStopping(early_stop)
+
+    if augmentation:
+        print('Augmenting data...')
+        train_dataset = TensorDataset(*augment_data(*train_dataset.tensors))
 
     if random_sampler:
         class_counts = torch.bincount(train_dataset.tensors[1].long())
@@ -103,7 +106,7 @@ def train_final_model(model,
 
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    curves = {'train_loss': [], 'val_loss': []}
+    curves = {'train_loss': [], 'val_loss': [], 'train_acc': [], 'val_acc': []}
     model_loss = []
 
     print('\nSetup finishied. Starting training...')
@@ -117,14 +120,13 @@ def train_final_model(model,
     for epoch in range(max_epochs):
         cumulative_train_loss = 0
         train_loss_count = 0
+        correct_train = 0
+        total_train = 0
 
         model.train()
         for i, (img, y_batch) in enumerate(train_loader):
 
             y_batch = y_batch.long()
-
-            if augmentation:
-                img, y_batch = augment_data(img, y_batch)
 
             if use_gpu:
                 img, y_batch = img.cuda(), y_batch.cuda()
@@ -140,7 +142,7 @@ def train_final_model(model,
             elif stage == 'rnn':
                 prediction = model.rnn_classifier(img)
                 loss = rnn_criterion(prediction, y_batch)
-
+                  
             else:
                 raise ValueError('Invalid stage value. Pick between "ae_alone", "ae_rnn" or "rnn".')
 
@@ -152,21 +154,40 @@ def train_final_model(model,
             cumulative_train_loss += loss.item()
             train_loss_count += 1
 
-            batch_losses = loss.item() 
-            model_loss.append(batch_losses)
+
+            # Calculate training accuracy
+            if stage == 'rnn':
+                _, predicted = torch.max(prediction, 1)
+                correct_train += (predicted == y_batch).sum().item()
+                total_train += y_batch.size(0)
+
+                batch_losses = loss.item()
+                model_loss.append(batch_losses)
+
+            else:
+                total_train = -1
+                batch_losses = loss.item() 
+                model_loss.append(batch_losses)
 
             if i > 0:
                 if (i % (n_batches // 100) == 0):
                     train_loss = cumulative_train_loss / train_loss_count
+                    
+                    train_acc = correct_train / total_train
 
-                    print(f"\r({((time.perf_counter() - t_i)/60):.2f} min) Epoch {epoch + 1}/{max_epochs} -- Iteration {iteration} - Batch {i}/{len(train_loader)} - Train loss: {train_loss:.8f} - Val loss: {val_loss:.8f}", end='')
+
+                    print(f"({((time.perf_counter() - t_i)/60):.2f} min) Epoch {epoch + 1}/{max_epochs} -- Iteration {iteration} - Batch {i}/{len(train_loader)} - Train loss: {train_loss:.8f}  - Train acc: {train_acc:.4f} - Val loss: {val_loss:.8f}")
 
             iteration += 1
 
         train_loss = cumulative_train_loss / train_loss_count
+        train_acc = correct_train / total_train
 
         #evaluation
         model.eval()
+        correct_val = 0
+        total_val = 0
+
         with torch.no_grad():
             img_val, y_val = next(iter(validation_loader))
             if use_gpu:
@@ -177,31 +198,39 @@ def train_final_model(model,
             if stage == 'ae_alone':
                 reconstruction_val = model.reconstruction(img_val)
                 val_loss = ae_criterion(reconstruction_val, img_val.view(-1, model.n_channels, 21, 21))
-
+                
             elif stage == 'ae_rnn':
                 reconstruction_val = model.rnn_encode(img_val)
                 val_loss = ae_criterion(reconstruction_val, img_val.view(-1, model.n_channels, 21, 21))
+                total_val = -1
 
             elif stage == 'rnn':
                 prediction_val = model.rnn_classifier(img_val)
                 val_loss = rnn_criterion(prediction_val, y_val)
+                
+                _, predicted_val = torch.max(prediction_val, 1)
+                correct_val += (predicted_val == y_val).sum().item()
+                total_val += y_val.size(0)
 
             else:
                 raise ValueError('Invalid stage value. Pick between "ae" and "rnn".')
 
+        val_acc = correct_val / total_val
 
         curves["train_loss"].append(train_loss)
         curves["val_loss"].append(val_loss)
+        curves["train_acc"].append(train_acc)
+        curves["val_acc"].append(val_acc)
 
-        print(f"\r({((time.perf_counter() - t_i)/60):.2f} min) Epoch {epoch + 1}/{max_epochs} -- Iteration {iteration} - Batch {i}/{len(train_loader)} - Train loss: {loss.item():.8f} - Val loss: {val_loss:.8f}", end='')
+        print(f"({((time.perf_counter() - t_i)/60):.2f} min) Epoch {epoch + 1}/{max_epochs} -- Iteration {iteration} - Batch {i}/{len(train_loader)} - Train loss: {train_loss:.8f}  - Train acc: {train_acc:.4f} - Val loss: {val_loss:.8f} - Val acc: {val_acc:.4f}")
 
         if early_stop != False:
             if early_stopping(val_loss):
-                print(f'\r({((time.perf_counter() - t_i)/60):.2f} min) Epoch {epoch + 1}/{max_epochs} (Early Stop) -- Iteration {iteration} - Batch {i}/{len(train_loader)} - Train loss: {loss.item():.8f} - Val loss: {val_loss:.8f}', end='')
+                print(f"({((time.perf_counter() - t_i)/60):.2f} min) Epoch {epoch + 1}/{max_epochs} -- Iteration {iteration} - Batch {i}/{len(train_loader)} - Train loss: {train_loss:.8f}  - Train acc: {train_acc:.4f} - Val loss: {val_loss:.8f} - Val acc: {val_acc:.4f}")
                 break
 
         if (time.perf_counter() - t_i)/60 > max_time:
-            print(f'\r({((time.perf_counter() - t_i)/60):.2f} min) Epoch {epoch + 1}/{max_epochs} (Max time) -- Iteration {iteration} - Batch {i}/{len(train_loader)} - Train loss: {loss.item():.8f} - Val loss: {val_loss:.8f}', end='')
+            print(f"({((time.perf_counter() - t_i)/60):.2f} min) Epoch {epoch + 1}/{max_epochs} -- Iteration {iteration} - Batch {i}/{len(train_loader)} - Train loss: {train_loss:.8f}  - Train acc: {train_acc:.4f} - Val loss: {val_loss:.8f} - Val acc: {val_acc:.4f}")
             break
 
     model.cpu()
